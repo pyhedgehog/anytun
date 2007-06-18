0f25d3ae3464ebe0710af31fea5c0fed8d6b1099
Christian Pointner <equinox@anytun.org>
2007-06-18 23:28:57 +0000

    yet another clean of main

--- anytun.cpp
+++ anytun.cpp
@@ -43,63 +43,43 @@ int main(int argc, char* argv[])
 {
   std::cout << "anytun - secure anycast tunneling protocol" << std::endl;
 
-//   TunDevice* dev;
+  TunDevice* dev;
 //   dev = new TunDevice("tun", "192.168.200.1", "192.168.201.1");
-//   std::cout << "dev created (opened)" << std::endl;
-//   std::cout << "dev opened - actual name is '" << dev->getActualName() << "'" << std::endl;
-//   std::cout << "dev type is '" << dev->getType() << "'" << std::endl;
-  
-//   sleep(10);
-  
-//   Buffer inBuf(2000);
-  
-//   while(1)
-//   {
-//     short revents = dev->read(inBuf);
-//     if(revents & POLLIN)
-//       std::cout << "POLLIN,";
-//     else if(revents & POLLRDNORM)
-//       std::cout << "POLLRDNORM,";
-//     else if(revents & POLLRDBAND)
-//       std::cout << "POLLRDBAND,";
-//     else if(revents & POLLPRI)
-//       std::cout << "POLLPRI,";
-//     else if(revents & POLLOUT)
-//       std::cout << "POLLOUT,";
-//     else if(revents & POLLWRNORM)
-//       std::cout << "POLLWRNORM,";
-//     else if(revents & POLLWRBAND)
-//       std::cout << "POLLWRBAND,";
-//     else if(revents & POLLERR)
-//       std::cout << "POLLERR,";
-//     else if(revents & POLLHUP)
-//       std::cout << "POLLHUP,";
-//     else if(revents & POLLNVAL)
-//       std::cout << "POLLNVAL,";
-//     std::cout << std::endl;
-//   }
-
-//   delete dev;
-//   std::cout << "dev destroyed" << std::endl;
-
-//   dev = new TunDevice("tap", "192.168.202.1", "255.255.255.0");
-//   std::cout << "dev created (opened)" << std::endl;
-//   std::cout << "dev opened - actual name is '" << dev->getActualName() << "'" << std::endl;
-//   std::cout << "dev type is '" << dev->getType() << "'" << std::endl;
-//   sleep(10);
-//   delete dev;
-//   std::cout << "dev destroyed" << std::endl;
-
-//   sleep(10);
-
+  dev = new TunDevice("tap", "192.168.202.1", "255.255.255.0");
 //   dev = new TunDevice("tun17", "192.168.200.1", "192.168.201.1");
-//   std::cout << "dev created (opened)" << std::endl;
-//   std::cout << "dev opened - actual name is '" << dev->getActualName() << "'" << std::endl;
-//   std::cout << "dev type is '" << dev->getType() << "'" << std::endl;
-//   sleep(10);
-//   delete dev;
-//   std::cout << "dev destroyed" << std::endl;
+  std::cout << "dev created (opened)" << std::endl;
+  std::cout << "dev opened - actual name is '" << dev->getActualName() << "'" << std::endl;
+  std::cout << "dev type is '" << dev->getType() << "'" << std::endl;
+  
+  Buffer inBuf(2000);
+  while(1)
+  {
+    short revents = dev->read(inBuf);
+    if(revents & POLLIN)
+      std::cout << "POLLIN,";
+    else if(revents & POLLRDNORM)
+      std::cout << "POLLRDNORM,";
+    else if(revents & POLLRDBAND)
+      std::cout << "POLLRDBAND,";
+    else if(revents & POLLPRI)
+      std::cout << "POLLPRI,";
+    else if(revents & POLLOUT)
+      std::cout << "POLLOUT,";
+    else if(revents & POLLWRNORM)
+      std::cout << "POLLWRNORM,";
+    else if(revents & POLLWRBAND)
+      std::cout << "POLLWRBAND,";
+    else if(revents & POLLERR)
+      std::cout << "POLLERR,";
+    else if(revents & POLLHUP)
+      std::cout << "POLLHUP,";
+    else if(revents & POLLNVAL)
+      std::cout << "POLLNVAL,";
+    std::cout << std::endl;
+  }
 
+  delete dev;
+  std::cout << "dev destroyed" << std::endl;
 
   return 0;
 }
