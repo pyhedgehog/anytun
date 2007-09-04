176b0249507dd592197661d8d53ffd91f49aad97
Othmar Gsenger <otti@anytun.org>
2007-09-04 17:38:06 +0000

    NULL String fixed in tundevice

--- tunDevice.cpp
+++ tunDevice.cpp
@@ -92,7 +92,7 @@ TunDevice::TunDevice(const char* dev_name,const char* dev_type, const char* ifcf
 //   lp = inet_addr("192.168.198.1");
 //   rp = inet_addr("192.168.199.1");
 
-  dev_ = init_tun(dev_name, dev_type, ifcfg_lp, ifcfg_rnmp, lp, rp, 0, NULL);
+  dev_ = init_tun(dev_name, dev_type==""?NULL:dev_type, ifcfg_lp, ifcfg_rnmp, lp, rp, 0, NULL);
   struct frame frame; // just for win32
   struct tuntap_options options; // win32 & linux
   options.txqueuelen = 100; // just for linux
