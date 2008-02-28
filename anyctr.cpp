adf2b0a7d5c96796d062628192e8a4dbf79c6fee
Othmar Gsenger <otti@anytun.org>
2008-02-28 22:05:01 +0000

    new sync format in anytun controller

--- anyctr.cpp
+++ anyctr.cpp
@@ -89,11 +89,11 @@ void createConnection(const std::string & remote_host, u_int16_t remote_port, Co
   const SyncCommand scom(cl,mux);
   const SyncCommand scom2 (prefix);
   oa << scom;
-  std::cout << sout.str() << std::endl;
+  std::cout <<  std::setw(5) << std::setfill('0') << sout.str().size()<< ' ' << sout.str() << std::endl;
 	std::ostringstream sout2;
 	boost::archive::text_oarchive oa2(sout2);
   oa2 << scom2;
-  std::cout << sout2.str() << std::endl;
+  std::cout <<  std::setw(5) << std::setfill('0') << sout2.str().size()<< ' ' << sout2.str() << std::endl;
 }
 
 int main(int argc, char* argv[])
