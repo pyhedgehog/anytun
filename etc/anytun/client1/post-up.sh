38d25b16bdab6b28dfcee1eedad95959de9e9a7c
Othmar Gsenger <otti@anytun.org>
2008-04-12 16:42:50 +0000

    removed non executeable scripts

--- etc/anytun/client1/post-up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-ip link set dev $1 up
-ip link set dev $1 mtu 1400
-ip addr add dev $1 192.168.123.1/24
-
-exit 0
--- etc/anytun/server/post-up.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-ip link set dev $1 up
-ip link set mtu 1400 dev $1
-ip addr add 192.168.123.254/24 dev $1
