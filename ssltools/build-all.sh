572823f65b365cd27345ee59d89b115df34c5338
Othmar Gsenger <otti@anytun.org>
2008-04-12 11:24:13 +0000

    svn cleanup

--- ssltools/build-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-. ./vars
-./easy-rsa/clean-all
-./easy-rsa/build-ca
-./easy-rsa/build-key server1
-./easy-rsa/build-key server2
-./easy-rsa/build-key server3
-./easy-rsa/build-key server4
