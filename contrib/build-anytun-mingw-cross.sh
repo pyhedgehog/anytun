5d773b252b9a8dce4c1e6481a3645ce31f27e244
Christian Pointner <equinox@anytun.org>
2012-01-06 02:49:55 +0000

    cleanup at ming-cross script for anytun

--- contrib/build-anytun-mingw-cross.sh
+++ contrib/build-anytun-mingw-cross.sh
@@ -3,16 +3,21 @@ set -e
 BASE=`pwd`
 
 cd ../src
-make distclean
-./configure --target=mingw --use-ssl-crypto --with-boost=../contrib/boost-w32 --with-openssl=../contrib/openssl-w32 --cross-prefix=i686-w64-mingw32-
-make -j 8
-make strip
-mkdir -p $BASE/anytun-w32
-cp *.exe $BASE/anytun-w32
-make distclean
-./configure --target=mingw --use-ssl-crypto --with-boost=../contrib/boost-w64 --with-openssl=../contrib/openssl-w64 --cross-prefix=x86_64-w64-mingw32-
-make -j 8
-make strip
-mkdir -p $BASE/anytun-w64
-cp *.exe $BASE/anytun-w64
+
+for target in w32 w64; do
+  if [ "$target" = "w32" ]; then
+    target_name=i686
+  else
+    target_name=x86_64
+  fi
+  make distclean
+  ./configure --target=mingw --use-ssl-crypto --with-boost=../contrib/boost-$target --with-openssl=../contrib/openssl-$target --cross-prefix=$target_name-w64-mingw32-
+  make
+  make strip
+  mkdir -p $BASE/anytun-$target
+  cp *.exe $BASE/anytun-$target
+done
+
 cd ../contrib
+
+exit 0
