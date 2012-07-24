ced887843ef7a33574121c3b76c09b21b0793e79
Christian Pointner <equinox@anytun.org>
2012-07-24 00:18:53 +0000

    added missing dll's to zip archives

--- contrib/make-mingw-release.sh
+++ contrib/make-mingw-release.sh
@@ -3,9 +3,17 @@
 VER=`cat ../version`
 
 for target in w32 w64; do
+  if [ "$target" = "w32" ]; then
+    LIBGCC_DLL=/usr/lib/gcc/i686-w64-mingw32/`i686-w64-mingw32-g++ -dumpversion`/libgcc_s_sjlj-1.dll
+    LIBSTDCPP_DLL=/usr/lib/gcc/i686-w64-mingw32/`i686-w64-mingw32-g++ -dumpversion`/libstdc++-6.dll
+  else
+    LIBGCC_DLL=/usr/lib/gcc/i686-w64-mingw32/`i686-w64-mingw32-g++ -dumpversion`/libgcc_s_sjlj-1.dll
+    LIBSTDCPP_DLL=/usr/lib/gcc/i686-w64-mingw32/`i686-w64-mingw32-g++ -dumpversion`/libstdc++-6.dll
+  fi
   mkdir anytun-$VER-$target
   cp anytun-$target/*.exe anytun-$VER-$target
-  cp boost-$target/lib/libboost_{date_time,serialization,system,thread_win32}.dll anytun-$VER-$target
+  cp $LIBGCC_DLL $LIBSTDCPP_DLL anytun-$VER-$target
+  cp boost-$target/lib/libboost_{date_time,serialization,system,thread_win32,chrono}.dll anytun-$VER-$target
   cp openssl-$target/bin/libeay32.dll anytun-$VER-$target
   cp anytun-example.bat anytun-$VER-$target
   cp ../{AUTHORS,ChangeLog,LICENSE,README,version} anytun-$VER-$target
